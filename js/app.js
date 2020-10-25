2b7306d32c8dc6ccf1e3a6dcecf5e170a7b36b4b
Tim <50641103+tikhusaenov@users.noreply.github.com>
2020-10-26 02:32:30 +0300

    added lights and shadows

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-let scene, camera, renderer, mesh
+let scene, camera, renderer, mesh, ambientLight, light
 let meshFloor
 let keyboard = {}
 
@@ -12,21 +12,35 @@ function init() {
     camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 0.1, 1000)
     mesh = new THREE.Mesh(
         new THREE.BoxGeometry(1,1,1),
-        new THREE.MeshBasicMaterial({color: 0xff9999, wireframe: false})
+        new THREE.MeshPhongMaterial({color: 0xff9999, wireframe: false})
     )
     scene.add(mesh)
     mesh.position.y += 1
 
     meshFloor = new THREE.Mesh(
         new THREE.PlaneGeometry(10,10, 10, 10),
-        new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: false})
+        new THREE.MeshPhongMaterial({color: 0xffffff, wireframe: false})
     )
     meshFloor.rotation.x -= Math.PI / 2
     scene.add(meshFloor)
+
+    ambientLight = new THREE.AmbientLight(0xffffff, 0.2)
+    scene.add(ambientLight)
+
+    light = new THREE.PointLight(0xffffff, 0.8, 18)
+    light.position.set(-3, 6, -3)
+    light.castShadow = true
+    light.shadow.camera.near = 0.1
+    light.shadow.camera.far = 25
+    scene.add(light)
+
     camera.position.set(0,player.height,-5)
     camera.lookAt(new THREE.Vector3(0,player.height,0))
+
     renderer = new THREE.WebGLRenderer()
     renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.shadowMap.enabled = true
+    renderer.shadowMap.type = THREE.BasicShadowMap
     document.body.appendChild(renderer.domElement)
 
     animate()
